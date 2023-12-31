019cf24458c37622573487d7e27757a84f8481ef
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-12-31 17:26:53 -0300

    pages params

---
+++ 2-nuxt-starter-pages/pages/ceps/[id].vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>/pages/ceps/index.vue</h1>
+        <h2>CEP Escolhido: {{ $route.params.id }}</h2>
+    </div>
+</template>
+  
---
+++ 2-nuxt-starter-pages/pages/ceps/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>/pages/ceps/index.vue</h1>
+
+        <div>{{ ceps }}</div>
+
+        <ul>
+            <li v-for="cep in ceps"><a :href="`/ceps/${cep}`">{{ cep }}</a> </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+    const ceps = [64034400,64039230]
+</script>
+  
