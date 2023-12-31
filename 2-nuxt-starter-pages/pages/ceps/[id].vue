5fd80c21fda8e6fa90b39e5dec23cf214d73aee6
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-12-31 18:22:09 -0300

    ceps

--- 2-nuxt-starter-pages/pages/ceps/[id].vue
+++ 2-nuxt-starter-pages/pages/ceps/[id].vue
@@ -3,9 +3,14 @@
         <h2><a :href="`/ceps`">Voltar</a></h2>
         <h1>/pages/ceps/index.vue</h1>
         <h2>CEP Escolhido: {{ $route.params.id }}</h2>
-
+        <h2>Variavel cepEscolhido</h2>
         <p>
             {{ cepEscolhido }}
+        </p>
+
+        <h2>Variavel mapeada - cep</h2>
+        <p>
+            {{ cep }}
         </p>
     </div>
 </template>
@@ -14,22 +19,47 @@
 
 import {ref} from 'vue';
 
-const cepsApi = [{ id: 64034400, logradouro: 'Rua das flores' }, { id: 64039230, logradouro: 'Avenida 7 de Setembro' }]
+const cepsApi = [{ id: 64000040, logradouro: 'Rua das flores' }, { id: 64000128, logradouro: 'Avenida 7 de Setembro' }]
 
 const { id } = useRoute().params //promisse + desestruturação (destruction)
 
 const cepEscolhido = ref({});
 
-onMounted(() => {
+const {data : cep} = await useFetch(`https://viacep.com.br/ws/${id}/json/`)
+
+cepEscolhido.value = cep;
+console.log('data foi', cep);
+ 
+/*
+(async () => {
+  try {
+    const response = await $fetch(`https://viacep.com.br/ws/${id}/json/`);
+    cepEscolhido.value = response.data; // Assuming your data is in response.data
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+onMounted( async () => {
     //console.log(cepsApi)
     console.log('CEP Escolhido para eu fazer qualquer coisa na minha lógica', id)
+    const { data } = await useFetch(`https://viacep.com.br/ws/${id}/json/`)
+    
+    console.log('data foi', data);
 
+    cepEscolhido.value = data;
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    
     //filter
+    
     cepEscolhido.value = cepsApi.find(function (el) {
         return el.id == id
     });
 
     console.log(cepEscolhido)
+    
 })
+*/
 </script>
   
--- 2-nuxt-starter-pages/pages/ceps/index.vue
+++ 2-nuxt-starter-pages/pages/ceps/index.vue
@@ -7,9 +7,44 @@
         <ul>
             <li v-for="cep in ceps"><a :href="`/ceps/${cep}`">{{ cep }}</a> </li>
         </ul>
+
+        <ul>
+            <li v-for="i in cepsObjects"><a :href="`/ceps/${i.id}`">{{ i.cep }} - {{ i  .logradouro }}</a> </li>
+        </ul>
     </div>
 </template>
 <script setup>
-    const ceps = [64034400,64039230]
+    const ceps = [64000040,64000128]
+
+    const cepsObjects = [
+
+    {
+   "id":64000040,     
+  "cep": "64000-040",
+  "logradouro": "Praça da Liberdade",
+  "complemento": "",
+  "bairro": "Centro",
+  "localidade": "Teresina",
+  "uf": "PI",
+  "ibge": "2211001",
+  "gia": "",
+  "ddd": "86",
+  "siafi": "1219"
+},
+{
+    "id":64000128,     
+    "cep": "64000-128",
+  "logradouro": "Rua Magalhães Filho",
+  "complemento": "(Zona Norte) - de 1/2 a 799/800",
+  "bairro": "Centro",
+  "localidade": "Teresina",
+  "uf": "PI",
+  "ibge": "2211001",
+  "gia": "",
+  "ddd": "86",
+  "siafi": "1219"
+}
+
+    ]
 </script>
   
