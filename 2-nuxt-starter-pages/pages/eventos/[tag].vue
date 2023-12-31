1a8f158cdeb77dadd09eab6d2c6f7fd6e95dd689
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-12-31 19:52:55 -0300

    {{ evento }}

--- 2-nuxt-starter-pages/pages/eventos/[tag].vue
+++ 2-nuxt-starter-pages/pages/eventos/[tag].vue
@@ -3,7 +3,7 @@
         <h2><NuxtLink to="/eventos"> Voltar </NuxtLink></h2>
         <h1>/pages/eventos/[ID].vue</h1>
         <h2>Evento Escolhido: {{ $route.params.tag }}</h2>
-        <h2>Evento:  {{ evento.tag }}</h2>
+        <h2>Evento:  {{ evento.id }}</h2>
         <p>
             {{ evento }}
         </p>
