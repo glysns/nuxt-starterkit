be4b022abf4e3420e7a1a1be86fba58b318cacca
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-12-31 17:52:45 -0300

    filtrando elementos num arrays

--- 2-nuxt-starter-pages/pages/ceps/[id].vue
+++ 2-nuxt-starter-pages/pages/ceps/[id].vue
@@ -1,7 +1,35 @@
 <template>
     <div>
+        <h2><a :href="`/ceps`">Voltar</a></h2>
         <h1>/pages/ceps/index.vue</h1>
         <h2>CEP Escolhido: {{ $route.params.id }}</h2>
+
+        <p>
+            {{ cepEscolhido }}
+        </p>
     </div>
 </template>
+
+<script setup>
+
+import {ref} from 'vue';
+
+const cepsApi = [{ id: 64034400, logradouro: 'Rua das flores' }, { id: 64039230, logradouro: 'Avenida 7 de Setembro' }]
+
+const { id } = useRoute().params //promisse + desestruturação (destruction)
+
+const cepEscolhido = ref({});
+
+onMounted(() => {
+    //console.log(cepsApi)
+    console.log('CEP Escolhido para eu fazer qualquer coisa na minha lógica', id)
+
+    //filter
+    cepEscolhido.value = cepsApi.find(function (el) {
+        return el.id == id
+    });
+
+    console.log(cepEscolhido)
+})
+</script>
   
