7ab51118a8b0a620fbdb1fcfb4b3f8461542152e
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2024-01-01 17:14:36 -0300

    Joao agora é com vc

--- 9-nuxt-casos-uso/reserva/pages/eventos/[id].vue
+++ 9-nuxt-casos-uso/reserva/pages/eventos/[id].vue
@@ -14,8 +14,7 @@
 
         <form>
             <input v-model="cadastro.nome" type="text" id="nome" name="nome" placeholder="Nome Completo" />
-            <input v-model="cadastro.email" type="text" id="email" name="email" placeholder="email@email.com"
-                @change="v$.email.$touch" />
+            <input v-model="cadastro.email" type="text" id="email" name="email" placeholder="email@email.com" />
             <input v-model="cadastro.cpf" type="text" id="cpf" name="cpf" placeholder="006.865.778-09" />
             <input v-model="cadastro.observacao" type="text" id="observacao" name="observacao"
                 placeholder="Entregar em: Rua das Marias, 375 - Teresina PI" />
@@ -27,39 +26,25 @@
 </template>
 
 <script setup>
-import { useVuelidate } from '@vuelidate/core';
-import { required, email, helpers } from '@vuelidate/validators';
-
 //https://rafamed.dev/blog/how-to-add-form-validation-to-your-nuxt-3-application
 const { evento, waiting, confirmarReserva } = useReservaService();
 const cadastro = ref({
-    nome: "Gleyson Sampaio",
-    cpf: "00432922326",
+    nome: "Gleyson Sampaio 2",
+    cpf: "19223219078",
+    email: "gleyson.me@gmail.com",
     observacao: "Rua das Marias, 375 - Teresina PI",
 });
-
-const rules = computed(() => {
-    return {
-        email: {
-            required: helpers.withMessage('O email é obrigatório', required),
-            email: helpers.withMessage('E-mail invalido', email),
-        }
-    };
-});
-
-const v$ = useVuelidate(rules, cadastro);
-
 const confirmar = async () => {
     const payload = cadastro.value;
-    if(!payload.email){
+    if (!payload.email) {
         alert('Email é obrigatório')
         return
     }
     console.log('cadastro preenchido', cadastro.value)
+    const { status, body } = await confirmarReserva(cadastro)
+    console.log('status', status);
+    console.log('body', body);
 
-    //const { status, body } = await confirmarReserva(cadastro)
-    //console.log('status', status);
-    //console.log('body', body);
 }
 </script>
   
