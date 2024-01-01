fbf0efc559565abb2a7a6b0de80ef72f68f0e722
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2024-01-01 17:09:14 -0300

    ATE TENTEI USAR VALIDADORES

--- 9-nuxt-casos-uso/reserva/pages/eventos/[id].vue
+++ 9-nuxt-casos-uso/reserva/pages/eventos/[id].vue
@@ -11,32 +11,55 @@
         </p>
 
         <p />
-        
-        <input v-model="cadastro.nome" type="text" id="nome" name="nome" placeholder="Nome Completo" />
-        <input v-model="cadastro.email" type="text" id="email" name="email" placeholder="email@email.com" />
-        <input v-model="cadastro.cpf"   type="text" id="cpf" name="cpf" placeholder="006.865.778-09" />
-        <input v-model="cadastro.observacao"   type="text" id="observacao" name="observacao" placeholder="Entregar em: Rua das Marias, 375 - Teresina PI" />
 
-        <h3 v-if="waiting">Processando ... </h3>
-        <button @click="confirmar()">Confirmar</button>
+        <form>
+            <input v-model="cadastro.nome" type="text" id="nome" name="nome" placeholder="Nome Completo" />
+            <input v-model="cadastro.email" type="text" id="email" name="email" placeholder="email@email.com"
+                @change="v$.email.$touch" />
+            <input v-model="cadastro.cpf" type="text" id="cpf" name="cpf" placeholder="006.865.778-09" />
+            <input v-model="cadastro.observacao" type="text" id="observacao" name="observacao"
+                placeholder="Entregar em: Rua das Marias, 375 - Teresina PI" />
+
+            <h3 v-if="waiting">Processando ... </h3>
+            <button @click.prevent="confirmar()">Confirmar</button>
+        </form>
     </div>
 </template>
 
 <script setup>
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, helpers } from '@vuelidate/validators';
+
 //https://rafamed.dev/blog/how-to-add-form-validation-to-your-nuxt-3-application
 const { evento, waiting, confirmarReserva } = useReservaService();
 const cadastro = ref({
     nome: "Gleyson Sampaio",
-    email: "gleyson.s@hotmail.com",
     cpf: "00432922326",
     observacao: "Rua das Marias, 375 - Teresina PI",
 });
 
+const rules = computed(() => {
+    return {
+        email: {
+            required: helpers.withMessage('O email é obrigatório', required),
+            email: helpers.withMessage('E-mail invalido', email),
+        }
+    };
+});
+
+const v$ = useVuelidate(rules, cadastro);
+
 const confirmar = async () => {
-    //confirmarReserva(cadastro)
-    const { status, body } = await confirmarReserva(cadastro)
-    console.log('status', status);
-    console.log('body', body);
+    const payload = cadastro.value;
+    if(!payload.email){
+        alert('Email é obrigatório')
+        return
+    }
+    console.log('cadastro preenchido', cadastro.value)
+
+    //const { status, body } = await confirmarReserva(cadastro)
+    //console.log('status', status);
+    //console.log('body', body);
 }
 </script>
   
