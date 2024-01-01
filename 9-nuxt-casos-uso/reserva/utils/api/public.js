06387511e2d11ac1c05fc2cf3d5775d1c796e970
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2024-01-01 12:02:29 -0300

    ENVS

--- 9-nuxt-casos-uso/reserva/utils/api/public.js
+++ 9-nuxt-casos-uso/reserva/utils/api/public.js
@@ -1,5 +1,6 @@
 export const usePublicApi = () => {
     const {api} = useApi();
+    
     const listEventos = async () => {
         return await useFetch(`https://iza-saas-api-production.up.railway.app/public/eventos`);
     }
