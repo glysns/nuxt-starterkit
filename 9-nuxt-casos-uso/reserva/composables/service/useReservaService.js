bf993960f8edaf7b9fd2745dded1ea32082879b8
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2024-01-01 12:54:15 -0300

    states

--- 9-nuxt-casos-uso/reserva/composables/service/useReservaService.js
+++ 9-nuxt-casos-uso/reserva/composables/service/useReservaService.js
@@ -1,13 +1,7 @@
 export const useReservaService = () => {
+    
     const evento = useState('evento',()=>{})
     const selecionarEvento = object => evento.value = object; 
-    /*
-    function selecionarEvento(eventoSelecionado){
-        evento.value = eventoSelecionado;
-    } 
-    */ 
-    return {
-        evento,
-        selecionarEvento
-    }
+    
+    return {evento,selecionarEvento}
 }
